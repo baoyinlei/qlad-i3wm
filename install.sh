85f86b3fa4f6aad607b03b04e49f10b4fd0d8667
qlad <3394543809@qq.com>
2021-05-03 00:01:54 +0800

    Setup script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+cp -rvp ./i3/ ~/.config/
+cp -rvp ./termite/ ~/.config/
+cp -rvp ./ranger/ ~/.config/
+cp -rvp ./polybar/ ~/.config/
+cp -vp ./.i3lock-gen-image.sh ~/
+cp -vp ./.sfw.sh ~/
+cp -vp ./.nsfw.sh ~/
+cp -vp ./.lockscreen.sh ~/
+cp -vp ./.start.sh ~/
+cp -vp ./.randomwallpaper.sh ~/
+cp -vp ./.start_conky.sh ~/
+cp -rvp ./.wallpaper/ ~/
+cp -rvp ./.wallpaper_nsfw/ ~/
+cp -rvp ./.conky_configs/ ~/
+echo "Done. please relogin into i3"
